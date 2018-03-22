c13f0f0cfaa343c3b01a22af40a60d721f878dfa
Jonathan Lee <jonathan.h.lee@gmail.com>
2018-03-22 04:23:20 -0700

    User profile completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def show
+    # @user = User.find(params[:id])
+    @user = current_user
+
+    if @user.nil?
+      redirect_to_new_user_registration_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  resources :users, only: [:show]
+
+  root 'users#show'
+
+  # root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
