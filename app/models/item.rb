4a827c6366e222aad4da158394f97df1cb73e63f
Jonathan Lee <jonathan.h.lee@gmail.com>
2018-04-04 03:48:06 -0700

    Time remaining added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  default_scope { order(created_at: :asc) }
+
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
 end
